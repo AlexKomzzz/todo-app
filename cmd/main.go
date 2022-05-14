837d853e828613fc5302746af2fb55387877dc05
alex <AlexKomzzz>
2022-05-14 13:09:50 +0300

    asd

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"log"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
-	mux := new(handler.Handler)
-	mux.InitRoutes().Run(":8000")
+	/*if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}*/
+
+	repos := repository.NewRepository() // Создание зависимостей
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	handlers = new(handler.Handler)
+	if err := handlers.InitRoutes().Run(":8000"); err != nil {
+		log.Fatalf("Error run web serv")
+	}
 }
+
+/*func initConfig() error { //Инициализация конфигураций
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}*/
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,13 +1,17 @@
 package handler
 
 import (
-	//"todo-app/pkg/service"
+	"todo-app/pkg/service"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
-	//service *service.Service
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine { // Инициализация групп функций мультиплексора
