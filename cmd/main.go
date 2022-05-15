24b6a735e4654e94fc4d6c438aa7aa6e545ebc42
alex <AlexKomzzz>
2022-05-15 21:54:17 +0300

    database connection

--- cmd/main.go
+++ cmd/main.go
@@ -5,25 +5,27 @@ import (
 	"todo-app/pkg/handler"
 	"todo-app/pkg/repository"
 	"todo-app/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	/*if err := initConfig(); err != nil {
+	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
-	}*/
+	}
 
 	repos := repository.NewRepository() // Создание зависимостей
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	handlers = new(handler.Handler)
-	if err := handlers.InitRoutes().Run(":8000"); err != nil {
+	if err := handlers.InitRoutes().Run(viper.GetString("port")); err != nil {
 		log.Fatalf("Error run web serv")
 	}
 }
 
-/*func initConfig() error { //Инициализация конфигураций
+func initConfig() error { //Инициализация конфигураций
 	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
-}*/
+}
